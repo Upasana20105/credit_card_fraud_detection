f2cc80a5f40d99add972af282f5653c4859b8564
Upasana20105 <146218471+Upasana20105@users.noreply.github.com>
2025-03-30 14:58:45 +0530

    Add files via upload

---
+++ creditcard/backend.py
@@ -0,0 +1,33 @@
+# flask_backend.py (Flask Backend)
+
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+model_path = "fraud_detection_model.joblib"
+
+try:
+    model = joblib.load(model_path)
+except Exception as e:
+    print(f"Error loading model: {e}")
+    model = None
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if model is None:
+        return jsonify({"error": "Model not loaded"}), 500
+
+    try:
+        data = request.get_json()
+        input_data = data['data']
+        input_array = np.array(input_data).reshape(1, -1) #Reshape for sklearn
+        prediction = model.predict(input_array)[0] #extract prediction
+
+        return jsonify({'prediction': int(prediction)})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
