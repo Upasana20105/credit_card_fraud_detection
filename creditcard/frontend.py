d4a9297fee2a713e60a92ddab3801a0a1737d8b5
Upasana Prajapati <146218471+Upasana20105@users.noreply.github.com>
2025-04-02 10:31:49 +0530

    Update frontend.py

--- creditcard/frontend.py
+++ creditcard/frontend.py
@@ -1,5 +1,3 @@
-# streamlit_frontend.py (Streamlit Frontend)
-
 import streamlit as st
 import requests
 import json
@@ -8,9 +6,7 @@ st.title("Credit Card Fraud Detection")
 
 # Input fields for credit card transaction data (adjust as needed)
 input_features = [
-    "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10",
-    "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20",
-    "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "Amount", "Time"
+    "V14", "V17", "V12", "V10", "Amount"
 ]
 
 input_values = []
